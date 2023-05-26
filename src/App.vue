0cfde8c86691d334d8d73a36a5ce1688aebf5962
Vikim_294 <1920829170@qq.com>
2023-05-26 17:29:55 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
